f50d06dcfb26907cec6c5ddce69a795b6f98c217
Ben Babtsov <ben.babtsov@ufl.edu>
2016-08-06 16:43:03 -0400

    Update 165_Compare_Version_Numbers.java

--- Leetcode/165_Compare_Version_Numbers.java
+++ Leetcode/165_Compare_Version_Numbers.java
@@ -10,3 +10,16 @@ public int compareVersion(String version1, String version2) {
     }
     return 0;
 }
+// without java 8:
+public int compareVersion(String version1, String version2) {
+    String[] v1 = version1.split("\\.");
+    String[] v2 = version2.split("\\.");
+    for (int i = 0, n = Math.max(v1.length, v2.length); i < n; i++) {
+        int a = (i < v1.length) ? Integer.parseInt(v1[i]): 0;
+        int b = (i < v2.length) ? Integer.parseInt(v2[i]): 0;
+        if (a != b) {
+            return (a > b) ? 1 : -1;
+        }
+    }
+    return 0;
+}
