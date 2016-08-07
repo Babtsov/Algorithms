828cb8c774fdc4343d56f1ed02263730616859ca
Ben Babtsov <ben.babtsov@ufl.edu>
2016-08-06 22:37:49 -0400

    Update 039_Combination_Sum.java

--- Leetcode/039_Combination_Sum.java
+++ Leetcode/039_Combination_Sum.java
@@ -2,36 +2,32 @@ public class Solution {
     private static class BackTrack {
         private int[] candidates;
         private List<List<Integer>> solutions;
-        private int target;
         
         public BackTrack(int[] candidates, int target) {
             this.candidates = candidates;
-            this.target = target;
             solutions = new ArrayList<List<Integer>>();
-            generateSolutions(0, 0, new Stack<Integer>());
+            generateSolutions(target, 0, new Stack<Integer>());
         }
         
         public List<List<Integer>> getSolutions() {
             return solutions;
         }
         
-        private void generateSolutions(int sum, int index, Stack<Integer> sequence) {
-            if (sum == target) {
-                ArrayList<Integer> list = new ArrayList<>();
-                list.addAll(sequence);
-                solutions.add(list);
-            } else if (sum < target) {
-                for (int i = index; i < candidates.length; i++) {
+        private void generateSolutions(int target, int index, Stack<Integer> sequence) {
+            if (target == 0) {
+                solutions.add(new ArrayList<>(sequence));
+            } else if (target > 0) {
+                for (int i = index; i < candidates.length && candidates[i] <= target; i++) {
                     sequence.push(candidates[i]);
-                    generateSolutions(sum + candidates[i], i,sequence);
+                    generateSolutions(target - candidates[i], i,sequence);
                     sequence.pop();
                 }
             }
         }
-        
     }
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
         return new BackTrack(candidates,target).getSolutions();
     }
 }
