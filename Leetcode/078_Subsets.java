b4bcbced8db83802df6d5a52629831e643cf461e
Ben Babtsov <ben.babtsov@ufl.edu>
2017-02-22 16:18:47 -0500

    Update 078_Subsets.java

--- Leetcode/078_Subsets.java
+++ Leetcode/078_Subsets.java
@@ -1,3 +1,4 @@
+// iterative solution
 public List<List<Integer>> subsets(int[] nums) {
     List<List<Integer>> solution = new ArrayList<List<Integer>>();
     for (int i = 0, subsetCount = 1 << nums.length; i < subsetCount; i++) {
@@ -9,5 +10,23 @@ public List<List<Integer>> subsets(int[] nums) {
         }
         solution.add(subset);
     }
+    return solution;
+}
+
+// recursive solution
+private void helper(List<List<Integer>> solution, ArrayList<Integer> subset, int[] nums, int index) {
+    if (index == nums.length) {
+        solution.add(subset);
+        return;
+    }
+    ArrayList<Integer> exclude = new ArrayList<Integer>(subset);
+    helper(solution, exclude, nums, index + 1);
+    ArrayList<Integer> include = new ArrayList<Integer>(subset);
+    include.add(nums[index]);
+    helper(solution, include, nums, index + 1);
+}
+public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> solution = new ArrayList<List<Integer>>();
+    helper(solution, new ArrayList<Integer>(), nums, 0);
     return solution;
 }
